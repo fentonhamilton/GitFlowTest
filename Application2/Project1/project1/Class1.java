672afb34466c29b76d152a8f2b0ba8b407911caa
hello@email.com <>
2017-11-22 07:29:31 +0000

    just a test

---
+++ Application2/Project1/project1/Class1.java
@@ -0,0 +1,12 @@
+package project1;
+
+public class Class1 {
+    public Class1() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        System.out.println("helllo");
+    }
+}
