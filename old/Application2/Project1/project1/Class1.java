ee57956906e522fcdeb095ce1014cc1594b11324
example <fenton.d.hamilton@accenture.com>
2018-01-19 17:12:55 +0000

    test residing folders

---
+++ old/Application2/Project1/project1/Class1.java
@@ -0,0 +1,16 @@
+package project1;
+
+public class Class1 {
+    public Class1() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        System.out.println("helllo 1234");
+
+        System.out.println("helllo 123456");
+        System.out.println("helllo 123457");
+        System.out.println("test");
+    }
+}
